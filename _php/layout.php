5be46ce77e8aeb0fcf5bd1dd87719f51882d9436
Marcel Varkevisser <32496414+marcelvark@users.noreply.github.com>
2019-01-20 00:58:50 +0100

    Update layout.php

--- _php/layout.php
+++ _php/layout.php
@@ -82,12 +82,6 @@
    <!-- Layout scripts ------------------------------------------------------->
 
    <script>
-      /*
-         Indicates whether the site is being served as static HTML.
-      */
-      const PiHPe = {
-         static: (Date.now() - <?= time() * 1000; ?> > 30000) // after 30s
-      }
    </script>
 
    <!-- Content scripts ------------------------------------------------------>
